03232064d91e7305d67a52f3d1ae860356ae20ed
Laura Pais <pais.laura@gmail.com>
2024-10-25 17:13:29 +0100

    ++

---
+++ script_v2.js
@@ -0,0 +1,16 @@
+const buttonStart = document.getElementById('buttonStart');
+const introSelvagem = document.getElementById('introSelvagem');
+const selvagemProgram = document.getElementById('selvagemProgram');
+
+buttonStart.addEventListener('click', () => {
+    introSelvagem.style.opacity = '0';
+
+    setTimeout(() => {
+        introSelvagem.style.display = 'none';
+        selvagemProgram.style.display = 'block';
+
+        setTimeout(() => {
+            selvagemProgram.style.opacity = '1';
+        }, 50); 
+    }, 700); 
+});
